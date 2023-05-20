dad2a4b212b6c52eaff5609959332e7fc3482183
gpsign <gpsignorini@gmail.com>
2023-05-20 01:21:01 -0300

    Otimizacoes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import { styled } from "styled-components";
 
+let px = 255 / screen.width;
+let py = 255 / screen.height;
+let pd = 255 / Math.sqrt(Math.pow(screen.width, 2), Math.pow(screen.height, 2));
+
 function App() {
 	const [coords, setCoords] = useState({ x: 0, y: 0 });
 
@@ -18,24 +22,21 @@ function App() {
 		};
 	}, []);
 
-	return <Corpo coords={coords}></Corpo>;
+	return (
+		<Corpo
+			r={Math.ceil(coords.x * px)}
+			g={Math.ceil(coords.y * py)}
+			b={
+				Math.ceil(Math.sqrt(Math.pow(coords.x, 2), Math.pow(coords.y, 2))) * pd
+			}
+		></Corpo>
+	);
 }
 
 const Corpo = styled.div`
 	width: 100vw;
 	height: 100vh;
-	background-color: ${({ coords }) =>
-		`rgb( ${Math.ceil(coords.x * (255 / screen.width))}, ${Math.ceil(
-			coords.y * (255 / screen.height)
-		)}, ${
-			255 /
-			Math.ceil(
-				Math.sqrt(
-					Math.pow(Math.ceil(coords.x * (255 / screen.width)), 2) +
-						Math.pow(Math.ceil(coords.y * (255 / screen.height)), 2)
-				)
-			)
-		})`};
+	background-color: ${({ r, g, b }) => `rgb( ${r}, ${g}, ${b})`};
 `;
 
 export default App;
